e75d77880d0e7a148089ebac6ae02226102cefc2
Siho Choi <sihochoii@gmail.com>
2022-08-04 22:29:41 -0700

    V2.1.0 Bug Fixes + Minor Addtions
    The NFC reading function has been fixed and tested.
    
    Bug Fixes:
    - Fixed decomposition error from structs (proper c implementation)
    - Changed siwtch cases to if statements since input data was String
    - Fixed function naming mistake in nfcRead function (Line: 256 recor.getPayloadLength(); --> record)
    - Removed accidental variable redeclarations
    - Fixed variable naming mistakes (Line 261 payLoadLength --> payloadLength)
    
    Minor Additions:
    - Added LCD libraries
    - Added LCD declarations
    - Added LCD startup animation
    - Added LCD update function
    - Added Buzzer declarations
    
    Urgent's:
    - Add serial status updates

--- KatGuard.ino
+++ KatGuard.ino
@@ -1,24 +1,55 @@
 
 // KatGuard v1.2
 #include <Wire.h>
+
+// NFC
 #include <PN532_I2C.h>
 #include <PN532.h>
 #include <NfcAdapter.h>
-
-// NFC
 PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
 byte nuidPICC[4];
 String masterPayload, masterUid;
 bool lockState = false;
+
 struct nfcKey
 {
-    String uid, payload;
-}
+    String uidData, payloadData;
+};
 
 // LCD
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+// Custom Characters
+byte locked[] = {
+  B01110,
+  B10001,
+  B10001,
+  B11111,
+  B11011,
+  B11011,
+  B11111,
+  B00000
+};
+
+byte unlocked[] = {
+  B01110,
+  B10010,
+  B10000,
+  B11111,
+  B11011,
+  B11011,
+  B11111,
+  B00000
+};
+
+// Light Sensor
+const int lightSensor = A0;
 
 // Buzzer
+#include "pitches.h"
+const int buz1 = 3;
 
 void setup(void)
 {
@@ -26,6 +57,7 @@ void setup(void)
     Serial.println("System initialized");
     nfc.begin();
     lcdUpdate("startup");
+    tone(buz1, NOTE_CS6, 100);
     lcdUpdate("unlocked");
 }
 
@@ -35,9 +67,14 @@ void loop(void)
     {
         if (nfc.tagPresent())
         {
-            nfcKey [masterUid, masterPayload] = readNFC();
+            nfcKey nfcKeyData = readNFC();
+            masterUid = nfcKeyData.uidData;
+            masterPayload = nfcKeyData.payloadData;
+            Serial.print("Master UID")
             lockState = true;
             lcdUpdate("locked");
+            tone(buz1, NOTE_CS5, 100);
+            tone(buz1, NOTE_CS6, 100);
         }
     }
     else
@@ -48,41 +85,118 @@ void loop(void)
             {
                 lockState = false;
                 lcdUpdate("unlocked");
+                masterUid = "NEKONEKONEKO";
+                masterPayload = "NEKONEKONEKO";
+                tone(buz1, NOTE_CS5, 100);
+                tone(buz1, NOTE_CS6, 100);
             }
         }
-        else (tarpStatus() == LOW)
-        {
-            alarmSystem(HIGH);
-            while (lockState == true)
-            {
-                // Maybe move to function?
-                if (nfc.tagPresent())
-                {
-                    if (authenticate(masterUid, masterPayload))
-                    {
-                        lockState = false;
-                        lcdUpdate("unlocked");
-                    }
-                }
-            }
-            alarmSystem(LOW);
-        }
+        // else (tarpStatus() == LOW)
+        // {
+        //     alarmSystem(HIGH);
+        //     while (lockState == true)
+        //     {
+        //         // Maybe move to function?
+        //         if (nfc.tagPresent())
+        //         {
+        //             if (authenticate(masterUid, masterPayload))
+        //             {
+        //                 lockState = false;
+        //                 lcdUpdate("unlocked");
+        //             }
+        //         }
+        //     }
+        //     alarmSystem(LOW);
+        // }
     }
     delay(100);
 }
 
 void lcdUpdate(String status)
 {
-    switch (status) {
-        case "startup":
-            // do something
-            break;
-        case "locked":
-            // do something
-            break;
-        case "unlocked":
-            // do something
-            break;
+    if (status == "startup")
+    {
+        lcd.begin();
+        lcd.backlight();
+        lcd.createChar(1, locked);
+        lcd.createChar(0, unlocked);
+        lcdAnimation(status);
+    }
+    else if (status == "locked")
+    {
+        printState("Locked", 1);
+    }
+    else if (status == "unlocked")
+    {
+        printState("Unlocked", 0);
+    }
+}
+
+void printState(char armTittle[], int iconState)
+{
+  // int = i;
+  lcd.setCursor(0,0);
+  lcd.write(iconState);
+  lcd.print(" ");
+  lcd.print(armTittle);
+  int blankSpace = 13 - strlen(armTittle);
+  for(int i = 0; i < blankSpace; i++)
+  {
+    lcd.print(" ");
+  }
+} 
+
+void lcdAnimation(String status)
+{
+    if (status == "startup")
+    {
+        lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print("^._.^");
+        delay(300);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("^._.^");
+        lcd.setCursor(0,1);
+        lcd.print("|   |");
+        delay(300);
+        lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print("^._.^ K");
+        delay(50);
+        lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print("^._.^ KA");
+        delay(50);
+        lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print("^._.^ KAT");
+        delay(50);
+        lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print("^._.^ KAT G");
+        delay(50);
+        lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print("|   | KAT GU");
+        lcd.setCursor(0,0);
+        lcd.print("^._.^");
+        delay(50);
+        lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print("|   | KAT GUA");
+        lcd.setCursor(0,0);
+        lcd.print("^._.^");
+        delay(50);
+        lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print("|   | KAT GUAR");
+        lcd.setCursor(0,0);
+        lcd.print("^._.^");
+        delay(50);
+        lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print("^._.^ KAT GUARD");
     }
 }
 
@@ -100,9 +214,8 @@ int tarpStatus()
 
 bool authenticate(String tagId, String payload)
 {
-    String testUid, testPayload;
-    nfcKey [testUid, testPayload] = readNFC;
-    if (testUid == tagId && testPayload == payload)
+    nfcKey nfcKeyData = readNFC();
+    if (nfcKeyData.uidData == tagId && nfcKeyData.payloadData == payload)
     {
         return true;
     }
@@ -112,23 +225,25 @@ bool authenticate(String tagId, String payload)
     }
 }
 
-nfcKey readNFC()
+struct nfcKey readNFC()
 {
-    String tagId = "None";
+    // String tagId = "None";
+    struct nfcKey nfcKeyData;
 
     NfcTag tag = nfc.read();
     Serial.println(tag.getTagType());
     Serial.print("UID: ");Serial.println(tag.getUidString());
     String tagId = tag.getUidString();
+    nfcKeyData.uidData = tagId;
 
     if (tag.hasNdefMessage())
     {
         Serial.println("nDef Message Found");
-        NdefMessage message = tag.NdefMessage();
+        NdefMessage message = tag.getNdefMessage();
         Serial.print("\nThis NFC Tag contains an NDEF Message with ");
         Serial.print(message.getRecordCount());
         Serial.print(" NDEF Record");
-        Serial.print(message.getRecordCount() != 1)
+        Serial.print(message.getRecordCount() != 1);
         {
             Serial.print("s");
         }
@@ -143,12 +258,12 @@ nfcKey readNFC()
             Serial.print("  TNF: ");Serial.println(record.getTnf());
             Serial.print("  Type: ");Serial.println(record.getType());
 
-            int payloadLength = recor.getPayloadLength();
+            int payloadLength = record.getPayloadLength();
             byte payload[payloadLength];
             record.getPayload(payload);
 
             Serial.print("  Payload (HEX): ");
-            PrintHexChar(payload, payLoadLength);
+            PrintHexChar(payload, payloadLength);
 
             String payloadAsString = "";
             for (int c = 0; c < payloadLength; c++)
@@ -157,13 +272,14 @@ nfcKey readNFC()
             }
             Serial.print(" Payload (as String): ");
             Serial.println(payloadAsString);
+            nfcKeyData.payloadData = payloadAsString;
 
             String uid = record.getId();
             if (uid != "")
             {
                 Serial.print("  ID: ");Serial.println(uid);
             }
-            return nfcKey {uid, payloadAsString};
+            return nfcKeyData;
         }
     }
 }
